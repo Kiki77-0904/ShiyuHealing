3c572d4ee51f1da7833a8f4c7b25d74f7ede7631
Ziqi Liao <3259837892@qq.com>
2026-04-15 23:27:16 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
-
+export default defineConfig({
+  plugins: [react()],
+  base: '/ShiyuHealing/',
+})
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
